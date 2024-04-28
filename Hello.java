a444dd833d365a12b6f8e085a164c3cc38c6d81d
ahmadgilang <ahmadgilang.app19@gmail.com>
2024-04-28 23:42:44 +0800

    Hello Git & java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
-    public static void main {string[]args}{
-        system.out.println("Hallo World !!")
+    public static void main (String[]args){
+        System.out.println("Hello Git");
+        System.out.println("Hello Java");
     }
 }
